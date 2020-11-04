747f4db946bf9bdbd55abddc6009c07fb701a388
arzhk <72197750+arzhk@users.noreply.github.com>
2020-11-04 15:14:50 +0000

    Update index.js
    Completed Ex. 4, 5 & 6

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 Starting from the current "base" bootstrap layout, implement the following exercise:
 1) When pressing on Load Images button, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your query DONE
 2) When pressing on Load Seconday Images, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your secondary query DONE
-3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view
-
-
-
-4) The Edit button should be replace with a "Hide" button. 
-5) When the hide button is pressed, the whole picture card disappears.
-6) Replace the "9 mins" string in the card template with the ID of the Image */
+3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view DONE
+4) The Edit button should be replace with a "Hide" button. DONE
+5) When the hide button is pressed, the whole picture card disappears. DONE
+6) Replace the "9 mins" string in the card template with the ID of the Image DONE */
 
 let imageLibrary = [];
 let imageLibrarySecondary = [];
 let allViewButtons = [];
+let allEditButtons = [];
 let imageSet_One;
 
 async function loadImage() {
@@ -23,8 +21,14 @@ async function loadImage() {
     imageLibrary.push(data);
     console.log("Loaded " + 10 + " images");
   });
+
+  const allImgIDs = document.querySelectorAll(".text-muted");
+  for (let i = 2; i < allImgIDs.length - 1; i++) {
+    allImgIDs[i].innerText = imageLibrary[0].images[i - 2].id;
+  }
+
   imageSet_One = true;
-  console.log(imageSet_One);
+  alert("Images have been loaded");
 }
 
 async function loadImagesSecondary() {
@@ -38,13 +42,11 @@ async function loadImagesSecondary() {
     });
   }
   imageSet_One = false;
-  console.log(imageSet_One);
+  alert("Images have been loaded");
 }
 
 function openModal() {
   const target = event.currentTarget;
-
-  console.log(imageSet_One);
 
   for (let i = 0; i < allViewButtons.length; i++) {
     if (imageSet_One === true) {
@@ -62,6 +64,12 @@ function openModal() {
   }
 }
 
+function hideCard() {
+  const targetCard =
+    event.currentTarget.parentNode.parentNode.parentNode.parentNode;
+  targetCard.style.display = "none";
+}
+
 function assignButtonIds() {
   const allButtons = document.querySelectorAll(".btn-outline-secondary");
 
@@ -71,12 +79,22 @@ function assignButtonIds() {
       allButtons[i].setAttribute("data-toggle", "modal");
       allButtons[i].setAttribute("data-target", "#exampleModal");
       allViewButtons.push(allButtons[i]);
+    } else {
+      allButtons[i].addEventListener("click", hideCard);
+      allEditButtons.push(allButtons[i]);
     }
   }
 }
 
+function replaceEditButtons() {
+  allEditButtons.forEach((e) => {
+    e.innerText = "Hide";
+  });
+}
+
 function start() {
   assignButtonIds();
+  replaceEditButtons();
 }
 
 window.onload = start();
