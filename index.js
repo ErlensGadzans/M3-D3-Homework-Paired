b679774cca4385b70c81a0fe92117d3eae6a2775
arzhk <72197750+arzhk@users.noreply.github.com>
2020-11-04 15:34:27 +0000

    Added functionality
    Completed Ex.8

--- index.js
+++ index.js
@@ -5,13 +5,42 @@ Starting from the current "base" bootstrap layout, implement the following exerc
 3) When the user clicks on the "VIEW" button inside the Card, open the specified image in a modal view DONE
 4) The Edit button should be replace with a "Hide" button. DONE
 5) When the hide button is pressed, the whole picture card disappears. DONE
-6) Replace the "9 mins" string in the card template with the ID of the Image DONE */
+6) Replace the "9 mins" string in the card template with the ID of the Image DONE
+
+[EXTRA]
+7) Add in the "jumbotron" a search field. If there is a value there and the user press "Load Seconday Image" the API call should use the specified query as query
+8) After every button is pressed, display in an alert for 5 seconds the result of the operation (es.: 20 images loaded)
+9) Handle API error gracefully using alert components with the message inside
+10) Add at the bottom of the page a carousel with "forest" images loaded by another API call
+
+[EVEN MORE EXTRA]
+11) Use the map method to create from your splashbase response object an array containing just the url strings
+12) Use filter to modify the "forest" api call to receive only images from a source different than "unsplash"
+13) Use the reduce method on the results array to sum up all the id numbers in a single one
+[HINT]
+You can replace the images src for making your pictures appear on button click or you can use template literals to re-create all the cards from scratch.
+Use arrow functions to make some practice with them
+API Docs: http://www.splashbase.co/api */
 
 let imageLibrary = [];
 let imageLibrarySecondary = [];
 let allViewButtons = [];
 let allEditButtons = [];
 let imageSet_One;
+
+function successAlert(numberOfImages) {
+  const alert = document.querySelector(".alert-success");
+  alert.classList.remove("d-none");
+  alert.classList.remove("slide-out-top");
+  alert.classList.add("d-block");
+  alert.classList.add("swing-in-top-fwd");
+  alert.innerText = `Successfully loaded ${numberOfImages} Images!`;
+
+  setTimeout(function () {
+    alert.classList.remove("d-block");
+    alert.classList.add("slide-out-top");
+  }, 3000);
+}
 
 async function loadImage() {
   const response = await fetch(
@@ -28,7 +57,7 @@ async function loadImage() {
   }
 
   imageSet_One = true;
-  alert("Images have been loaded");
+  successAlert(10);
 }
 
 async function loadImagesSecondary() {
@@ -46,7 +75,7 @@ async function loadImagesSecondary() {
     allImgIDs[i].innerText = imageLibrarySecondary[i].id;
   }
   imageSet_One = false;
-  alert("Images have been loaded");
+  successAlert(10);
 }
 
 function openModal() {
